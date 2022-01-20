12483a7552e439fa8837a493ec5ed85a6578f410
kb3 <krishdevops012>
2022-01-20 22:40:20 +0530

    my name is krishna

---
+++ Hello.py
@@ -0,0 +1,15 @@
+print(" Hello krishna, welcome to python project")
+a=2
+b=4
+print(a+b)
+
+x=6
+r=x % 2
+if r ==0:
+    print('even')
+    if x<=7:
+        print('Great')
+    else:
+        print('not great')
+else:
+    print('Odd')
